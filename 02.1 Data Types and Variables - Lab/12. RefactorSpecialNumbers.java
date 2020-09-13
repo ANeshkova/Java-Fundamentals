6a008b1b8d96d4b9d27d35f64b18bde02a7d2f39
ANeshkova <anitowurth@abv.bg>
2020-09-13 13:52:17 +0300

    Added solution

---
+++ 02.1 Data Types and Variables - Lab/12. RefactorSpecialNumbers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class RefactorSpecialNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int number = Integer.parseInt(scan.nextLine());
+
+        for (int ch = 1; ch <= number; ch++) {
+            int sum = 0;
+            int digit = ch;
+            while (digit > 0) {
+                sum += digit % 10;
+                digit = digit / 10;
+            }
+
+            if (sum == 5 || sum == 7 || sum == 11) {
+                System.out.printf("%d -> True%n", ch);
+            } else {
+                System.out.printf("%d -> False%n", ch);
+            }
+        }
+    }
+}
