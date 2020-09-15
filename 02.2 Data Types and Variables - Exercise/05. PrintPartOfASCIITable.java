ba3465be1f9b478c35c571eed2c5fa35e0ffedc6
ANeshkova <anitowurth@abv.bg>
2020-09-15 19:03:48 +0300

    Added solution

---
+++ 02.2 Data Types and Variables - Exercise/05. PrintPartOfASCIITable.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class PrintPartOfASCIITable {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int start = Integer.parseInt(scan.nextLine());
+        int finish = Integer.parseInt(scan.nextLine());
+
+        for (int i = start; i <= finish; i++) {
+            System.out.print((char)i + " ");
+        }
+    }
+}
