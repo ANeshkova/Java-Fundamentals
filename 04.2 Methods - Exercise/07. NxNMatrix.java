70c042c2875df109425d90362e3dc35675b2515a
ANeshkova <anitowurth@abv.bg>
2020-09-24 15:17:25 +0300

    Added solution

---
+++ 04.2 Methods - Exercise/07. NxNMatrix.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class NxNMatrix {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        printMatrix(n);
+    }
+
+    public static void printMatrix(int n) {
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                System.out.print(n + " ");
+            }
+            System.out.println();
+        }
+    }
+}
